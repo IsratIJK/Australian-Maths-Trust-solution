ba93a1d06b8c59429f9252c9e6abab5f67b0f000
IsratIJK <iptu.jahan.amin@gmail.com>
2021-02-11 21:05:02 +0600

    solved the first problem

---
+++ Addition.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h> // include a lot of functionality of C++. This is a combination of many libraries
+using namespace std;
+int main() {
+    freopen("addin.txt" , "r", stdin);
+    freopen("addout.txt" , "w", stdout);
+    
+    int a,b,sum;
+    cin >> a >> b;
+    sum=a+b;
+    cout << sum << endl;
+    return 0;
+}
